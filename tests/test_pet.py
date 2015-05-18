a558e37b96d0f93849a5987125ddba34845aa3d0
Kenneth Love <kenneth@gigantuan.net>
2015-05-18 13:06:51 -0700

    cleanup

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,16 +1,15 @@
 from pets import Dog
 
+rover = Dog(name='Rover', sound='bark', age=3)
+chinese = Dog(name='Chow', sound='汪汪', age=10)
 
 def test_dog():
-    rover = Dog(name='Rover', sound='bark', age=3)
     assert rover.name == 'Rover'
 
 
 def test_dog_call():
-    kalb = Dog(name='كلب', sound='هو, هو', age=10)
     assert kalb.come_here() == 'Here, كلب, here!'
 
 
 def test_dog_age():
-    spike = Dog(name='Spike', age=14)
     assert spike.dog_years == 2
